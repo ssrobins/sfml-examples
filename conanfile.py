37a9b6a7b7f74eae47b8ec9c756b38466f16c69a
Steve Robinson <ssrobins@gmail.com>
2019-08-14 20:59:10 -0700

    Use recipe revision instead of user and channel

--- conanfile.py
+++ conanfile.py
@@ -6,5 +6,5 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("box2d/2.3.1@stever/stable")
-        self.requires.add("sfml/2.5.1@stever/stable")
+        self.requires.add("box2d/2.3.1#a0215989fbf5f43230376a078b276eaa8b3d4474")
+        self.requires.add("sfml/2.5.1#820f9ee26eecf951975a053826974787eab9ac82")
