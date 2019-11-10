ae40c3c5fd5bf45b4c93ceb870b20633659ab17e
Steve Robinson <ssrobins@gmail.com>
2019-11-10 10:16:21 -0800

    Fix missing assets in the installs on non-Apple platforms

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires.add("cmake_utils/0.1.0#7f17deeced79eecd4a03ba2d327bee3e5e794732")
+        self.build_requires.add("cmake_utils/0.1.0#89a9bc6923fea4dabf19de6aa6b7ddcf8667b2ee")
 
     def requirements(self):
         self.requires.add("box2d/2.3.1#dc184ca0366ec14bd0e85f8b21b1e32f46a570d4")
