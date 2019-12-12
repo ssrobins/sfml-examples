1d2e9cdc3bd1b1af644381dd9ba8d7adb0210ed5
Steve Robinson <ssrobins@gmail.com>
2019-12-11 23:21:54 -0800

    Update CMake minimum

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires.add("cmake_utils/0.1.0#89a9bc6923fea4dabf19de6aa6b7ddcf8667b2ee")
 
     def requirements(self):
-        self.requires.add("box2d/2.3.1#dc184ca0366ec14bd0e85f8b21b1e32f46a570d4")
-        self.requires.add("sfml/2.5.1#ce4fde128151ffee338a5de73c6fff1e9f0d2d1f")
+        self.requires.add("box2d/2.3.1#18cbe3ca36efde484001aeca05edfa62cdc0bcfd")
+        self.requires.add("sfml/2.5.1#63650f391c1574648fbf72b307fa9d35caf869b4")
