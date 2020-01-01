7eb95aef1ada85488e5616ac7a97672b3ec494f7
Steve Robinson <ssrobins@gmail.com>
2020-01-01 13:12:52 -0800

    Fix the build error from the previous commit by updating Conan packages so that the macOS packages have os.version set to 10.9

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires.add("cmake_utils/0.3.1#1cf9333e6fba1b7350ec8d4d06f737b54d163eef")
 
     def requirements(self):
-        self.requires.add("box2d/2.3.1#18cbe3ca36efde484001aeca05edfa62cdc0bcfd")
-        self.requires.add("sfml/2.5.1#63650f391c1574648fbf72b307fa9d35caf869b4")
+        self.requires.add("box2d/2.3.1#722f09428568a828755cbac453888cce327681c7")
+        self.requires.add("sfml/2.5.1#658a32038bba095fa6bd3c48165321e0c9d90569")
