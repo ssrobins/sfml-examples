b09680fede8359fe27d1bd17e587194163ed2604
Steve Robinson <ssrobins@gmail.com>
2020-03-12 22:47:42 -0700

    Upgrade Conan packages

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#1cf9333e6fba1b7350ec8d4d06f737b54d163eef")
+        self.build_requires("cmake_utils/0.3.1#bddead915084dbfac2b4114574fae5ae8545e540")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#722f09428568a828755cbac453888cce327681c7")
-        self.requires("sfml/2.5.1#658a32038bba095fa6bd3c48165321e0c9d90569")
+        self.requires("box2d/2.3.1#9699e70bd278099f8da60ae508796a598ce159dd")
+        self.requires("sfml/2.5.1#b0d57764841bf37146f6d7c4ed3f2a18ae85ccc7")
