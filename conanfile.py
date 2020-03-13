2e4fe2708d8d9f5dd74e1a8549c955cbaf03d7ec
Steve Robinson <ssrobins@gmail.com>
2020-03-12 17:50:40 -0700

    Use documented calls for build_requires and requires

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires.add("cmake_utils/0.3.1#1cf9333e6fba1b7350ec8d4d06f737b54d163eef")
+        self.build_requires("cmake_utils/0.3.1#1cf9333e6fba1b7350ec8d4d06f737b54d163eef")
 
     def requirements(self):
-        self.requires.add("box2d/2.3.1#722f09428568a828755cbac453888cce327681c7")
-        self.requires.add("sfml/2.5.1#658a32038bba095fa6bd3c48165321e0c9d90569")
+        self.requires("box2d/2.3.1#722f09428568a828755cbac453888cce327681c7")
+        self.requires("sfml/2.5.1#658a32038bba095fa6bd3c48165321e0c9d90569")
