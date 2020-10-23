6d14b0b0ce26abdeea226e66a3e26080605ca5f7
Steve Robinson <ssrobins@gmail.com>
2020-10-22 22:38:40 -0700

    Upgrade freetype

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#e259c69dc653c346b70350d21113a2adaad72079")
-        self.requires("sfml/2.5.1#cd092e0ab5f37bceb1601ea5021068a84afd934c")
+        self.requires("box2d/2.3.1#e86e6c95a7193c4654700665f93ea446969c711f")
+        self.requires("sfml/2.5.1#53b19f48090b9597c7b7bf76ae29e8e80df924e7")
