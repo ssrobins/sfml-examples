172c314fbe4c9102828f57303cfa0438443ce29b
Steve Robinson <ssrobins@gmail.com>
2021-02-14 22:06:10 -0800

    Switch from bintray to artifactory for Conan packages

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
+        self.build_requires("cmake_utils/0.3.1#e474aafdec36cf92d97e781b844f390f3170f29f")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#e86e6c95a7193c4654700665f93ea446969c711f")
-        self.requires("sfml/2.5.1#53b19f48090b9597c7b7bf76ae29e8e80df924e7")
+        self.requires("box2d/2.3.1#2d87b135821108cab9473d63a7b67ad0b925bca3")
+        self.requires("sfml/2.5.1#088b79f2ab521b0cf6cf5d745b862147b265b2d5")
