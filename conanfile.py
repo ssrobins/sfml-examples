4940382ee37b9ccf90eb4db48893d80bb1bfecca
Steve Robinson <ssrobins@gmail.com>
2021-07-11 20:38:59 -0700

    Updating packages after consolidating Conan scripts

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#2640730b74de43c61ce2c6f209c570b2b05ceeec")
-        self.requires("sfml/2.5.1#60a8805c66f3d2339b63a6b14c7203e564b36955")
+        self.requires("box2d/2.3.1#5818ed1c3a8557baaf3ac244553c4aaf1987458c")
+        self.requires("sfml/2.5.1#9e7d536ae87aa4d222de81824ad84049aede3646")
