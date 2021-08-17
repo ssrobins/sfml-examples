1d3c0f2d3d61d5c7baebd3b3668a92767e1e3fc6
Steve Robinson <ssrobins@gmail.com>
2021-08-16 23:03:24 -0700

    Update packages after Android NDK update

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#5818ed1c3a8557baaf3ac244553c4aaf1987458c")
-        self.requires("sfml/2.5.1#9e7d536ae87aa4d222de81824ad84049aede3646")
+        self.requires("box2d/2.3.1#00a7a8fbcd0665f2e547aab9d6acdaf73aafe0fd")
+        self.requires("sfml/2.5.1#14616b0363a0677811713e413226212e46e44bf0")
