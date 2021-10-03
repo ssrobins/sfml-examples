4d1b20ed7733f24a36a7b0e29fc42e29658ed1c7
Steve Robinson <ssrobins@gmail.com>
2021-10-02 19:29:19 -0700

    Add an option to build Conan packages if they aren't on the package server

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
+        self.build_requires("cmake_utils/0.3.1#a1d53d179d9736ff032b5f5de3e4c3e2eebcb1f0")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#00a7a8fbcd0665f2e547aab9d6acdaf73aafe0fd")
-        self.requires("sfml/2.5.1#14616b0363a0677811713e413226212e46e44bf0")
+        self.requires("box2d/2.3.1#c9ad941f2cc9b02c162942cc6af9859e6397ed05")
+        self.requires("sfml/2.5.1#68c0f11697a80f04e14b505cfba7610b028b9d9f")
