b8987dc1e14c843f049a8dd4576a1d769037cea8
Steve Robinson <ssrobins@gmail.com>
2021-10-09 00:51:23 -0700

    Upgrade Xcode

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#a1d53d179d9736ff032b5f5de3e4c3e2eebcb1f0")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#c9ad941f2cc9b02c162942cc6af9859e6397ed05")
-        self.requires("sfml/2.5.1#68c0f11697a80f04e14b505cfba7610b028b9d9f")
+        self.requires("box2d/2.3.1#c9e861a6615050fb060773f070123f48c8417033")
+        self.requires("sfml/2.5.1#ada4c4c4ec7dfab4aed45311e4487aab625aca38")
