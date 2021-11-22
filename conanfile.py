c934c671fe9e87e04164cfff0b69c9cf4da0d907
Steve Robinson <ssrobins@gmail.com>
2021-11-21 18:17:59 -0800

    Use Ninja-built macOS package

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/2.0.1#bc87acc9a67867fb20e22e3c51eb4c070a9f9758")
+        self.build_requires("cmake_utils/3.0.0#4e7b4d9bfca394477325cdfc8eacce8b1c82583e")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#8d35fff9bc17cf629dd8fab8babc2a3f7cdc3228")
-        self.requires("sfml/2.5.1#f57c2ff1310d9691428e0149cdd1c4ed7768a739")
+        self.requires("box2d/2.3.1#c578e6a30b02cb10d4195b7d1d8798a47d604f37")
+        self.requires("sfml/2.5.1#69412c151d6eef5d35cea61894bb72b4da33d29c")
