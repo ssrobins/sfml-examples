4f9dcf3310ca8019cb3323386831f8d8fcc17745
Steve Robinson <ssrobins@gmail.com>
2022-02-21 00:45:40 -0800

    Set PRODUCT_BUNDLE_IDENTIFIER to fix macOS build warning

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/3.0.0#4e7b4d9bfca394477325cdfc8eacce8b1c82583e")
+        self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#c578e6a30b02cb10d4195b7d1d8798a47d604f37")
-        self.requires("sfml/2.5.1#69412c151d6eef5d35cea61894bb72b4da33d29c")
+        self.requires("box2d/2.3.1#c5ab311eb0ab814da6173d00ced8dc187229a3e0")
+        self.requires("sfml/2.5.1#7d6d90d08596afbb245ef8cc27809fea69851193")
