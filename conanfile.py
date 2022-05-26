802b2093ec712f811caf86a28071a5da5ea49df0
Steve Robinson <ssrobins@gmail.com>
2022-05-25 20:39:43 -0700

    Convert to Conan's CMakeDeps generator

--- conanfile.py
+++ conanfile.py
@@ -2,10 +2,10 @@ from conans import ConanFile
 
 class Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/8.0.0#b0932588b4ccd8ac8db4fed90295f7480ac0b37f")
+        self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
 
     def requirements(self):
         self.requires("box2d/2.3.1#7be42ea3ee83f5e3d80d9b9513e11f15092e4f47")
