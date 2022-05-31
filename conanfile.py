4aece185ee51c95a270b071f5321ef748a2ef52a
Steve Robinson <ssrobins@gmail.com>
2022-05-30 23:55:46 -0700

    Use Conan packages made with the CMakeDeps generator

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#7be42ea3ee83f5e3d80d9b9513e11f15092e4f47")
-        self.requires("sfml/2.5.1#1230d76d351f88b81dc25050e6e76f1a7e5d356c")
+        self.requires("box2d/2.3.1#449fcf50a8576d57f1541acd4ce71298039ba282")
+        self.requires("sfml/2.5.1#0e0eec0ebbdd0304915592e218ca25b2f48c7cff")
