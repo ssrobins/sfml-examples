75da563c81d8f99089a0e13ec6204c21a3236894
Steve Robinson <ssrobins@gmail.com>
2022-05-31 12:50:36 -0700

    Fix arch in iOS Conan profile

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#449fcf50a8576d57f1541acd4ce71298039ba282")
-        self.requires("sfml/2.5.1#0e0eec0ebbdd0304915592e218ca25b2f48c7cff")
+        self.requires("box2d/2.3.1#246411ef22a5bd9a9526f74068e6f6acba420f72")
+        self.requires("sfml/2.5.1#d75c42001d70f0cd03a0a967f7a69951d5672787")
