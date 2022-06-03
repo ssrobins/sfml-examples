cab6c2a08899da37f02ec181f124c5535a685330
Steve Robinson <ssrobins@gmail.com>
2022-06-02 18:41:49 -0700

    Update Conan packages to reference conan-recipes repo sha

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class Conan(ConanFile):
     generators = "CMakeDeps"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
+        self.build_requires("cmake_utils/9.0.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
 
     def requirements(self):
-        self.requires("box2d/2.3.1#246411ef22a5bd9a9526f74068e6f6acba420f72")
-        self.requires("sfml/2.5.1#d75c42001d70f0cd03a0a967f7a69951d5672787")
+        self.requires("box2d/2.3.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
+        self.requires("sfml/2.5.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
