8052d7da40dee7456c8cf2f20715b05d5c1776b4
Steve Robinson <ssrobins@gmail.com>
2022-06-11 16:51:14 -0700

    Update packages after (mostly) migrating to Conan 2.0 (#9)

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,7 @@ class Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
 
-    def build_requirements(self):
-        self.build_requires("cmake_utils/9.0.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
-
     def requirements(self):
-        self.requires("box2d/2.3.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
-        self.requires("sfml/2.5.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
+        self.requires("box2d/2.3.1#16c0041381708e6564b0a9a8ba11b0b0bd067fa2")
+        self.requires("cmake_utils/9.0.1#16c0041381708e6564b0a9a8ba11b0b0bd067fa2")
+        self.requires("sfml/2.5.1#16c0041381708e6564b0a9a8ba11b0b0bd067fa2")
