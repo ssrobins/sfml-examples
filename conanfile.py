40a7bc3cd593af949a0f9702acc7e9d8234c2d04
Steve Robinson <ssrobins@gmail.com>
2022-06-27 19:09:48 -0700

    Switch to Conan 2.0 and use package 'user'

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,12 @@
-from conans import ConanFile
+from conan import ConanFile
+
+required_conan_version = ">=2.0.0-beta1"
 
 class Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
 
     def requirements(self):
-        self.requires("box2d/2.3.1#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
-        self.requires("cmake_utils/10.0.1#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
-        self.requires("sfml/2.5.1#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
+        self.requires("box2d/2.3.1@ssrobins")
+        self.requires("cmake_utils/10.0.1@ssrobins")
+        self.requires("sfml/2.5.1@ssrobins")
